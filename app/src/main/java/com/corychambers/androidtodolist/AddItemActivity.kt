10461c5bdbbdbd93a7b4f2b6dfd551de4d7df6e1
Cory Chambers <corymchambers1@gmail.com>
2021-01-17 10:04:59 -0300

    Added connections from layout to activity

--- app/src/main/java/com/corychambers/androidtodolist/AddItemActivity.kt
+++ app/src/main/java/com/corychambers/androidtodolist/AddItemActivity.kt
@@ -1,11 +1,31 @@
 package com.corychambers.androidtodolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.EditText
 
 class AddItemActivity : AppCompatActivity() {
+
+    private lateinit var addTodoName: EditText
+    private lateinit var urgentCheck: CheckBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_item)
+
+        addTodoName = findViewById(R.id.add_todo_name)
+        urgentCheck = findViewById(R.id.urgent_check)
+    }
+
+    public fun cancelPressed(view: View) {
+        var intent: Intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    public fun savePressed(view: View) {
+
     }
 }
